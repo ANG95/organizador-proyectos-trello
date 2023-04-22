e9ebdb6ce6a9a6b393cfdd1801268251b9aa1cbd
German Apaza <german.apaza@lya-electronic.com>
2023-04-22 18:10:37 -0500

    fix: delete uncessary comments

--- TrelloApp/src/screens/board/components/boardColumn/boardColumn.tsx
+++ TrelloApp/src/screens/board/components/boardColumn/boardColumn.tsx
@@ -13,24 +13,26 @@ const BoardColumn = ({ item }: BoardColumnProps) => {
     <View style={styles.columnContainer}  >
       <Text style={styles.columnLabel}>{item.name}</Text>
       <Divider y={5} />
-      <ScrollView
-        contentContainerStyle={{}}
-        showsVerticalScrollIndicator={false}
-      >
-        {item.boardItems.map((itemBoard) => (
-          <View
-            style={styles.cardItem}
-            key={itemBoard.id}
-          >
-            <Text style={styles.cardLabel}>{itemBoard.name}</Text>
-          </View>
-        ))}
-        <TouchableOpacity style={styles.addCardButton}        >
-          <SvgIcon icon="circlePlus" widthScale={15} heightScale={18} />
-          <Divider x={5} />
-          <Text style={styles.cardLabel}>Agregar card</Text>
-        </TouchableOpacity>
-      </ScrollView>
+      <View>
+        <ScrollView
+          contentContainerStyle={{}}
+          showsVerticalScrollIndicator={false}
+        >
+          {item.boardItems.map((itemBoard) => (
+            <View
+              style={styles.cardItem}
+              key={itemBoard.id}
+            >
+              <Text style={styles.cardLabel}>{itemBoard.name}</Text>
+            </View>
+          ))}
+          <TouchableOpacity style={styles.addCardButton}        >
+            <SvgIcon icon="circlePlus" widthScale={15} heightScale={18} />
+            <Divider x={5} />
+            <Text style={styles.cardLabel}>Agregar card</Text>
+          </TouchableOpacity>
+        </ScrollView>
+      </View>
     </View>
   )
 }

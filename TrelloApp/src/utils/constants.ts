8f42f33ba5b195cc7298f977005507fc99eae7ec
German Apaza <german.apaza@lya-electronic.com>
2023-04-22 12:41:13 -0500

    feature: add constants (evironments)
    estas constantes deben ir como variables de entorno

--- TrelloApp/src/utils/constants.ts
+++ TrelloApp/src/utils/constants.ts
@@ -1,1 +1,4 @@
-export const BASE_URL = 'http:trello.com'
+// this constants confire in .env
+export const BASE_URL = 'https://api.trello.com/1'
+export const API_KEY = '8cbd01ee687df7d5eaf53de6cb1662f6';
+export const TOKEN = 'ATTA5121b7176a8c501941278d50e8e5fd32c59a57273d297098337dcbd148076f56E4588436'

36724ac85a4053f0f3c0a450a6df07b5324ff244
German Apaza <german.apaza@lya-electronic.com>
2023-04-21 20:20:21 -0500

    feature: add theme provider app

---
+++ TrelloApp/src/theme/colorTheme.ts
@@ -0,0 +1,18 @@
+import { palette } from './paletteTheme';
+
+export const Color = {
+  palette,
+  backgroundPrimary: palette.white,
+  backgroundInput: palette.greyFourth,
+  primary: palette.lightBlue,
+  primaryText: palette.greyPrimary,
+  secondaryText: palette.greySecondary,
+  tertiaryText: palette.greyTertiary,
+  fourthText: palette.greyFourth,
+  error: palette.error,
+  info: palette.info,
+  warning: palette.warning,
+  success: palette.success,
+  disabled: palette.grey,
+  separatorBorder: palette.greyQuinary
+};
